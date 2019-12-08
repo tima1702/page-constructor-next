e39d6aa842d13c07f43d7079dc0bbf2701820852
Timofey Kuznetsov <tima1702@gmail.com>
2019-12-08 18:04:24 +0300

    Add service worker

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withSass = require('@zeit/next-sass');
+const withOffline = require('next-offline');
 
-module.exports = withSass({
-  devIndicators: {
-    autoPrerender: false,
-  },
-});
+module.exports = withOffline(
+  withSass({
+    devIndicators: {
+      autoPrerender: false,
+    },
+  })
+);
 
